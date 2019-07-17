b2fb5a2631a8908d8aa7970643db4288e25d6691
Konstantin <konstantin.saigushev.com>
2019-07-17 22:13:37 +0600

    base TicTacToe class structure

--- src/TicTacToe.php
+++ src/TicTacToe.php
@@ -5,8 +5,29 @@ namespace App;
 
 use App\strategies\Easy;
 use GuzzleHttp\Client;
+use App\TicTacToeAlgoritmInterface;
 
 class TicTacToe
 {
+
+    private $canvas = [ 
+        1 => null, 2 => null, 3 => null,
+        4 => null, 5 => null, 6 => null,
+        7 => null, 8 => null, 9 => null
+    ];
+
+    private $algorithm;
+
+    public function __construct(TicTacToeAlgoritmInterface $algorithm = null)
+    {
+        $this->algorithm = $algorithm ?? new Easy();
+    }
+
+    public function go(int $row = null, int $column = null)
+    {
+        $newCanvasState = ( $row && $column ? $this->makeStep($row, $column) : $this->algorithm->makeStep() );
+
+        $this->canvas = array_intersect_key($newCanvasState, $this->canvas);
+    }
 
 }
